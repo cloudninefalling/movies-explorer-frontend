6389e38fe5672174e34ced9f4b58958446376ccd
cloudninefalling <shalygin.da7@gmail.com>
2024-02-07 13:54:41 +0300

    fix: link to / from ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,5 +2,5 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ element, isLoggedIn }) {
-  return isLoggedIn ? element : <Navigate to="/signin" replace />;
+  return isLoggedIn ? element : <Navigate to="/" replace />;
 }
